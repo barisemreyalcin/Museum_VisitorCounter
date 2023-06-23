df056dde094b3177226d1289c29211fe5db480e3
Barış Emre Yalçın <barisemre.bey@gmail.com>
2023-06-23 21:44:12 +0300

    save button functionality: completed

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 let visitors = 0;
 let counter = document.querySelector(".counter");
+let entHistory = document.querySelector(".entry-history");
 
 function increment() {
     visitors++;
+    counter.innerText = visitors;
+}
+
+function save() {
+    let countStr = visitors + " - ";
+    entHistory.textContent += countStr;
+    visitors = 0;
     counter.innerText = visitors;
 }
